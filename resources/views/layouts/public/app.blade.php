ef6be0fe1bd6c4c27eb460066b9050d8f98a5d97
Sergio Alejandro Corcuera Mel <sergio.corcuera@munsurco.gob.pe>
2024-05-14 15:15:41 -0500

    fc

---
+++ resources/views/layouts/public/app.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@stack('title') | {{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="{{asset('assets/css/global/fonts.css')}}">
+
+    {{-- Icons --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome/all.min.css') }}">
+
+    {{-- Personalized styles --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/global/style.css') }}">
+
+    @stack('css')
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div id="app">
+        <main class="py-4">
+            @yield('content')
+        </main>
+
+        <script src="{{ asset('assets/js/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('assets/js/fontawesome/all.min.js') }}"></script>
+
+        {{-- Personalized JS --}}
+        @stack('js')
+    </div>
+</body>
+
+</html>
