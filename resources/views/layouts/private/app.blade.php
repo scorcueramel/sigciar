e1ac68d5738e119f0e2715c1625ee8e6a7a77dae
Sergio Alejandro Corcuera Mel <sergio.corcuera@munsurco.gob.pe>
2024-05-16 22:12:17 -0500

    16052024

--- resources/views/layouts/private/app.blade.php
+++ resources/views/layouts/private/app.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="utf-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
@@ -22,10 +23,11 @@
     {{-- FullCalendar --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.5/main.min.css" integrity="sha256-EAq+X/hXd44MlSOCkm9hchPJT78vQ4UBTT7FBkQl9qE=" crossorigin="anonymous">
 
-    @stack('css')
 
     {{-- Personalized --}}
     <link rel="stylesheet" href="{{ asset('assets/css/personalized/style.css') }}">
+
+    @stack('css')
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
@@ -89,15 +91,15 @@
         <main>
             @yield('content')
         </main>
-        <!-- JQuery -->
+        {{-- <!-- JQuery --> --}}
         <script src="{{ asset('assets/js/jquery/jquery.min.js') }}"></script>
         {{-- Fontawesome Pro --}}
         <script src="{{ asset('assets/js/fontawesome/all.min.js') }}"></script>
         {{-- MomentJS (locale date) --}}
-        <script src="https://momentjs.com/downloads/moment-with-locales.min.js"></script>
+        <script src="{{asset('assets/js/fullcalendar/moment-with-locales.min.js')}}"></script>
         {{-- FullCalendar Free --}}
-        <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.5/main.min.js" integrity="sha256-gqhxNoRraWeCppNZHezKGig+uPdmR/cntAtJsdTkm5A=" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.5/locales-all.min.js" integrity="sha256-lomiTyENeSGOpm5TiwjdxUn87bSv1TL581KZ7bhEEh0=" crossorigin="anonymous"></script>
+        <script src="{{asset('assets/js/fullcalendar/main.min.js')}}"></script>
+        <script src="{{asset('assets/js/fullcalendar/locales-all.min.js')}}"></script>
         {{-- Sweetalert2 --}}
         <script src="{{ asset('assets/js/sweetalert/sweetalert2@11.js') }}"></script>
         {{-- Personalized JS --}}
--- resources/views/layouts/public/app.blade.php
+++ resources/views/layouts/public/app.blade.php
@@ -4,28 +4,21 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>@stack('title') | {{ config('app.name', 'Laravel') }}</title>
-
     <!-- Fonts -->
     <link rel="stylesheet" href="{{asset('assets/css/global/fonts.css')}}">
-
     {{-- Icons --}}
     <link rel="stylesheet" href="{{ asset('assets/css/fontawesome/all.min.css') }}">
-
     {{-- Personalized styles --}}
     <link rel="stylesheet" href="{{ asset('assets/css/global/style.css') }}">
-
     {{-- FullCalendar --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.5/main.min.css" integrity="sha256-EAq+X/hXd44MlSOCkm9hchPJT78vQ4UBTT7FBkQl9qE=" crossorigin="anonymous">
-
-    @stack('css')
-
     {{-- Personalized --}}
     <link rel="stylesheet" href="{{ asset('assets/css/personalized/style.css') }}">
+
+    @stack('css')
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
@@ -36,15 +29,15 @@
         <main>
             @yield('content')
         </main>
-        {{-- JQuery --}}
+        {{-- <!-- JQuery --> --}}
         <script src="{{ asset('assets/js/jquery/jquery.min.js') }}"></script>
         {{-- Fontawesome Pro --}}
         <script src="{{ asset('assets/js/fontawesome/all.min.js') }}"></script>
         {{-- MomentJS (locale date) --}}
-        <script src="https://momentjs.com/downloads/moment-with-locales.min.js"></script>
+        <script src="{{asset('assets/js/fullcalendar/moment-with-locales.min.js')}}"></script>
         {{-- FullCalendar Free --}}
-        <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.5/main.min.js" integrity="sha256-gqhxNoRraWeCppNZHezKGig+uPdmR/cntAtJsdTkm5A=" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.5/locales-all.min.js" integrity="sha256-lomiTyENeSGOpm5TiwjdxUn87bSv1TL581KZ7bhEEh0=" crossorigin="anonymous"></script>
+        <script src="{{asset('assets/js/fullcalendar/main.min.js')}}"></script>
+        <script src="{{asset('assets/js/fullcalendar/locales-all.min.js')}}"></script>
         {{-- Sweetalert2 --}}
         <script src="{{ asset('assets/js/sweetalert/sweetalert2@11.js') }}"></script>
         {{-- Personalized JS --}}
