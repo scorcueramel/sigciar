d42e8b3734a7238ca1fd943245b80a3087a7c1d4
scorcueramel <46231549+scorcueramel@users.noreply.github.com>
2024-05-20 12:23:58 -0500

    20052024

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Lugar;
 use App\Models\Persona;
 use App\Models\Sede;
+use App\Models\Servicio;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -18,20 +19,22 @@ class ReservationController extends Controller
         $authenticate = false;
         $personalInfo = null;
 
-        if(Auth::check()){
+        if (Auth::check()) {
             $authenticate = true;
-            $personalInfo = Persona::where('usuario_id',Auth::user()->id)->select('nombres','apepaterno','apematerno')->get();
+            $personalInfo = Persona::where('usuario_id', Auth::user()->id)->select('nombres', 'apepaterno', 'apematerno')->get();
         }
-        $sede = Sede::where('estado','A')->select('id','descripcion','abreviatura','estado')->get();
-        return view('pages.public.reservation.index',compact('sede','personalInfo','authenticate'));
+        $sede = Sede::where('estado', 'A')->select('id', 'descripcion', 'abreviatura', 'estado')->get();
+        return view('pages.public.reservation.index', compact('sede', 'personalInfo', 'authenticate'));
     }
 
-    public function getPlaces($id){
-        $lugares = Lugar::where('sede_id',$id)->get();
+    public function getPlaces($id)
+    {
+        $lugares = Lugar::where('sede_id', $id)->get();
         return response()->json($lugares);
     }
 
-    public function dateQuery(Request $request){
+    public function dateQuery(Request $request)
+    {
         $fechasRecibidas = $request->all();
         $message = "Este horario no se encuentra disponible, te sugerimos seleccionar otro.";
 
@@ -39,25 +42,25 @@ class ReservationController extends Controller
         $end = $fechasRecibidas["end"]; //desde el calendario
         $fechasAlmacenadas = DB::select("SELECT s.inicio, s.fin FROM servicios s");
 
-        $fecstart = substr($start,0,10) . " " . substr($start,11,8); //formatear fecha recibida del calendario
-        $fecend = substr($end,0,10) . " " . substr($end,11,8); //formatear fecha recibida del calendario
+        $fecstart = substr($start, 0, 10) . " " . substr($start, 11, 8); //formatear fecha recibida del calendario
+        $fecend = substr($end, 0, 10) . " " . substr($end, 11, 8); //formatear fecha recibida del calendario
 
         foreach ($fechasAlmacenadas as $key => $value) {
             $fechaStart = $fechasAlmacenadas[$key]->inicio; //fechas obtenidas de la BD
             $fechaEnd = $fechasAlmacenadas[$key]->fin; //fechas obtenidas de la BD
 
             //fechavista    //fechabd
-            if($fecstart <= $fechaStart && $fecend > $fechaStart){
-                return response()->json(["msg"=>$message]);
+            if ($fecstart <= $fechaStart && $fecend > $fechaStart) {
+                return response()->json(["msg" => $message]);
             }
-            if($fecstart < $fechaEnd && $fecend > $fechaEnd){
-                return response()->json(["msg"=>$message]);
+            if ($fecstart < $fechaEnd && $fecend > $fechaEnd) {
+                return response()->json(["msg" => $message]);
             }
-            if($fecstart >= $fechaStart && $fecend <= $fechaEnd){
-                return response()->json(["msg"=>$message]);
+            if ($fecstart >= $fechaStart && $fecend <= $fechaEnd) {
+                return response()->json(["msg" => $message]);
             }
         }
-        return response()->json(["msg"=>"ok"]);
+        return response()->json(["msg" => "ok"]);
     }
 
     public function create()
@@ -76,10 +79,10 @@ class ReservationController extends Controller
     public function show()
     {
         //
-        $reservations = DB::select('SELECT
-        sr.id, sr.servicioplantilla_id, sr.inicio as "start", sr.fin as "end", sr.estado, sr.usuario_creador, sr.usuario_editor,
-        sr.ip_usuario, sr.deleted_at, sr.created_at, sr.updated_at
-        FROM servicioreserva sr');
+        $reservations = DB::select('SELECT sr.id, sr.servicioplantilla_id, sr.inicio as "start", sr.fin as "end",
+        sr.estado, sr.usuario_creador, sr.usuario_editor,
+        sr.usuario_ip , sr.deleted_at, sr.created_at, sr.updated_at
+        FROM servicio_reservas sr');
 
         return response()->json($reservations);
     }
