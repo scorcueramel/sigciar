591d7614fdc1084d4b2ac4cd80e0c083577cbf9a
scorcueramel <46231549+scorcueramel@users.noreply.github.com>
2024-08-21 09:17:42 -0500

    21082024

--- app/Http/Controllers/CalendarioGeneral.php
+++ app/Http/Controllers/CalendarioGeneral.php
@@ -25,42 +25,25 @@ class CalendarioGeneral extends Controller
         return response()->json($lugares);
     }
 
-    public function chargeEvents(Request $request)
+    public function chargeEvents()
     {
-        $sedeView = $request->sede;
-        $lugarView = $request->lugar;
-        $tiposervicioView = $request->tiposervicio;
-
-        if ($sedeView == null && $lugarView == null && $tiposervicioView == null) {
-            $reservas = $this->getCitas(0,0,0);
-            return response()->json($reservas);
-        }
-
-        if($sedeView == null && $lugarView == null && $tiposervicioView != null){
-            $reservas = $this->getCitas($tiposervicioView,0,0);
-            return response()->json($reservas);
-        }
-
-        if($sedeView != null && $lugarView == null && $tiposervicioView == null){
-            $reservas = $this->getCitas(0,$sedeView,0);
-            return response()->json($reservas);
-        }
-
-        if($sedeView == null && $lugarView != null && $tiposervicioView == null){
-            $reservas = $this->getCitas(0,0,$lugarView);
-            return response()->json($reservas);
-        }
-
-        if($sedeView != null && $lugarView != null && $tiposervicioView != null){
-            $reservas = $this->getCitas($tiposervicioView,$sedeView,$lugarView);
-            return response()->json($reservas);
-        }
+        $reservas = $this->getCitas(0, 0, 0);
+        return response()->json($reservas);
     }
 
-    protected function getCitas($sede, $lugar, $tiposervicio)
+    public function chargeEventsQuery($tiposervicioView,$sedeView,$lugarView)
+    {
+        $sedeView == 0 || $sedeView == null ? 0 : $sedeView;
+        $lugarView == 0 || $lugarView == null ? 0 : $lugarView;
+        $tiposervicioView  == 0 || $tiposervicioView == null ? 0 : $tiposervicioView ;
+        $reservas = $this->getCitas($tiposervicioView, $sedeView , $lugarView);
+        return response()->json($reservas);
+    }
+
+    protected function getCitas($tiposervicio, $sede, $lugar)
     {
         $reservas = [];
-        $inscritos = DB::select("SELECT tiposervicio_id, sede_id, lugar_id, start, ends as end, nombre, movil, email, categoria FROM calendario_listar(?,?,?)", [$sede, $lugar, $tiposervicio]);
+        $inscritos = DB::select("SELECT tiposervicio_id, sede_id, lugar_id, start, ends as end, nombre, movil, email, categoria FROM calendario_listar(?,?,?)", [$tiposervicio, $sede, $lugar]);
 
         foreach ($inscritos as $inscrito) {
             $fecha = Str::before($inscrito->start, " ");
